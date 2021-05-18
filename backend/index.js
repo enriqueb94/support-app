50696559206f36d46ff08f8e832722c680bf6313
quique <enriquebdfx@gmail.com>
2021-05-18 11:06:53 -0400

    configurando mongo atlas connection

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,10 @@
 var mongoose = require('mongoose');
 var app = require('./app');
 var port = 3700;
+var uri = 'mongodb+srv://user_1:NjGReyr9qIf9J1t1@cluster0.dm3gz.mongodb.net/supportContact?retryWrites=true&w=majority';
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/supportContact',{
+mongoose.connect(uri,{
     useUnifiedTopology: true,
 	useNewUrlParser: true,
 	useFindAndModify: false
@@ -13,7 +14,7 @@ mongoose.connect('mongodb://localhost:27017/supportContact',{
 .then(() =>{
     console.log('Conexion a la BD establecida');
     app.listen(port, () => {
-        console.log('Servidor corriendo correctamente en http://localhost:3700');
+        console.log('Servidor corriendo correctamente');
     })
 })
 .catch(error => {
